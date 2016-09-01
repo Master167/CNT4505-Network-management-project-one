741bd1f4511bc28d2dc373f1baf97b72d69f4428
Michael Frederick <michaelfrederick167@gmail.com>
2016-09-01 10:07:04 -0400

    Generate Netbeans projects
    Main classes for each side is ClientSide and ServerSide

---
+++ ServerSide/src/serverside/ServerSide.java
@@ -0,0 +1,12 @@
+package serverside;
+
+public class ServerSide {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
